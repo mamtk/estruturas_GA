1a9bffbdb5959882df86448d2383c615b0e6abb0
Morris <mamtk@users.noreply.github.com>
2016-04-03 00:02:36 -0300

    Fun with a C++11 functional observer.

--- Simulator.hh
+++ Simulator.hh
@@ -1,7 +1,43 @@
 #pragma once
 
+
+/*
+ * Simulator: basicamente um factory method para SessionMon e Station,
+ *  que instancia as classes de simulação
+ */
 class Simulator {
 
+};
+
+// inspiração para o observer estilo C++11 obtida de:
+//  https://juanchopanzacpp.wordpress.com/2013/02/24/simple-observer-pattern-implementation-c11/
+class ClockObserver;
+
+class ClockSubject {
+	using Observer = std::function<void(char)> const&;
+	using ObserverP = void(*)(char);
+public:
+
+	void registerObserver(Observer observer) {
+	  _mObservers.push_back(std::forward<Observer>(observer));
+	}
+
+	void unregisterObserver(ObserverP observer) { // tão funcional, tão hipster...
+		// se eu pudesse usar C++14...
+		//_mObservers.erase(std::remove_if(_mObservers.begin(), _mObservers.end(), [&_mObservers](const auto &x)
+		_mObservers.erase(std::remove_if(_mObservers.begin(), _mObservers.end(), [observer](Observer x)
+		{
+			void (*const* ptr)(char) = x.target<void(*)(char)>();
+			return (ptr && *ptr == observer);
+		}), _mObservers.end());
+	}
+
+	void notify() const	{
+	  for (const auto& o : _mObservers) o('u');
+	}
+
+   private:
+	std::vector<std::function<void(char)>> _mObservers;
 };
 
 class SimulatorClock {
--- main.cc
+++ main.cc
@@ -1,9 +1,33 @@
 #include <iostream>
+#include <functional>
 
+#include <vector>
+#include <algorithm>
+#include "Simulator.hh"
 using namespace std;
+
+void update1(char a)
+{
+	std::cout << "Update1" << std::endl;
+}
+
+void update2(char a)
+{
+	std::cout << "Update2" << std::endl;
+}
+
 
 int main(int argc, char *argv[])
 {
-	cout << "Hello World!" << endl;
+	ClockSubject cs;
+	cs.registerObserver(update1);
+	cs.registerObserver(update1);
+	cs.registerObserver(update1);
+	cs.registerObserver(update2);
+	cs.notify();
+	cs.unregisterObserver(update1);
+	std::cout << std::endl;
+	cs.notify();
+
 	return 0;
 }
